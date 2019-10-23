0a1b6ad694fb2032a7686b81577adae4cd50e409
andrew-k-smith <andrewsmith1025@gmail.com>
2019-10-23 14:23:41 -0400

    testing statements

--- src/main.cpp
+++ src/main.cpp
@@ -5,27 +5,47 @@ using namespace std;
 
 int main(int argc, char const *argv[])
 {
-    cout << "Main method\n";
     myLList ll;
-    ll.add(1);
-    cout << ll.getHead() << endl;
-    cout << ll.getTail() << endl;
-    ll.add(2);
-    cout << ll.getHead() << endl;
-    cout << ll.getTail() << endl;
-    ll.add(3);
-    cout << ll.getHead() << endl;
-    cout << ll.getTail() << endl;
-
-    cout << ll.find(2) << endl;
-
-    // Node *temp;
-    // temp = &n1;
-    // cout <<(*temp).getData() << endl;
-    // temp = (*temp).getNext();
-    // cout << (*temp).getData();
-
-
-
+    cout << "Creating new LL called ll" << endl;
+    float anArray[20];
+    for(int i=0;i<20;i++){
+        anArray[i] = i;
+        ll.add(i);
+    }
+    cout << "Printing ll" << endl;
+    ll.print();
+    ll.remove(10);
+    cout << "Size of ll is: " << ll.getSize() << endl;
+    cout << "Printing ll" << endl;
+    ll.print();
+    ll.insert(4.5, 5);
+    cout << "Printing ll" << endl;
+    ll.print();
+    cout << "Size of ll is: " << ll.getSize() << endl;
+    myLList ll1;
+    cout << "Creating new LL called ll1" << endl;
+    for(int i=0;i<5;i++){
+        ll1.add(i);
+    }
+    cout << "Size of ll1 is: " << ll1.getSize() << endl;
+    cout << "Printing ll1" << endl;
+    ll1.print();
+    cout << "Concatenating ll1 onto ll" << endl;
+    ll+ll1;
+    cout << "Size of ll is: " << ll.getSize() << endl;
+    cout << "Printing ll" << endl;
+    ll.print();
+    cout << "Printing ll1" << endl;
+    ll1.print();
+    cout << "Setting ll1 equal to ll" << endl;
+    ll1=ll;
+    cout << "Size of ll1 is: " << ll1.getSize() << endl;
+    cout << "Size of ll is: " << ll.getSize() << endl;
+    cout << "Printing ll" << endl;
+    ll.print();
+    cout << "Printing ll1" << endl;
+    ll1.print();
+    myLList ll2(sizeof(anArray)/sizeof(*anArray), anArray);
+    ll2.print();
     return 0;
 }
