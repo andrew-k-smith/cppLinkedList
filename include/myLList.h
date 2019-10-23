72bb72c972a1516eb53edab0c683c1ae5c2abcba
andrew-k-smith <andrewsmith1025@gmail.com>
2019-10-23 14:22:56 -0400

    added methods to obj

--- include/myLList.h
+++ include/myLList.h
@@ -9,12 +9,21 @@ class myLList
 {
 public:
     myLList();
+    myLList(int, float *anArray);
     myLList(const myLList &);
     ~myLList();
+    void operator+(const myLList &);
+    void operator=(const myLList &); 
+    Datum operator[](int);
+    bool operator==(const myLList &); 
     void add(float aData);
+    void insert(float, int);
+    void remove(int index);
+    void print();
     Datum *find(float aData);
     Datum *getHead();
     Datum *getTail();
+    int getSize();
 
 private:
     Datum *head;
